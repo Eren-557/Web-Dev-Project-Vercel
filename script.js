a101064a2a339454eed2d8506786a72e9cc6a7af
eren <eren.akatsuki7@gmail.com>
2026-06-16 23:37:45 +0530

    commit

--- script.js
+++ script.js
@@ -1,211 +1,217 @@
-const board = document.querySelector('.board');
-const startButton = document.querySelector('.btn-start');
-const restartButton = document.querySelector('.btn-restart');
+const board = document.querySelector('.board')
 
-const modal = document.querySelector('.modal');
-const startGameModal = document.querySelector('.start-game');
-const gameOverModal = document.querySelector('.game-over');
-const blockHeight = 50;
-const blockWidth = 50;
+let blockHeight ;
+let blockWidth ;
+if (window.innerWidth < 768) {
+    blockHeight = 40;
+    blockWidth = 40;  // Mobile value
+} else {
+    blockHeight = 50; // Desktop value
+    blockWidth = 50; 
+}
 
-let highscoreElement = document.querySelector('#high-score');
-let scoreElement = document.querySelector('#score');
-let timeElement = document.querySelector('#time');
 
-const cols = Math.floor(board.clientWidth / blockWidth);
-const rows = Math.floor(board.clientHeight / blockHeight);
+// modal
+const modal = document.querySelector(".modal");
+const startGameModal = document.querySelector(".start-game");
+const endGameModal = document.querySelector(".game-over");
+
+// gameDifficulty buttons
+const gameDifficulty = document.querySelector(".game-difficulty");
+const easybtn = document.querySelector(".easy-btn");
+const mediumbtn = document.querySelector(".medium-btn");
+const hardbtn = document.querySelector(".hard-btn");
+
+// rows and column
+const cols = Math.floor(board.clientWidth/blockWidth);
+const rows = Math.floor(board.clientHeight/blockHeight);
+
+const blocks = {};
 
 let intervalId = null;
 let timerintervalId = null;
-
-const blocks = [];
-
-let food =  {x: Math.floor(Math.random()*rows),y: Math.floor(Math.random()*cols)}
-
-let snake = [
-    {
-        x:1,
-        y:3
-    }
-
-]
-
 let direction = 'down'
 
-let highScore = localStorage.getItem("highScore") || 0;
-let score = 0
-let time = `00-00`
+//snake and food coordinates
+let snake = [{x: 1,y: 3}]
 
-highscoreElement.textContent = highScore;
+let food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)}
 
+// game infos
+let highscoreElement = document.querySelector("#highs-score");
+let scoreElement = document.querySelector("#score");
+let timeElement = document.querySelector("#time");
 
-// for(let i = 0; i < rows * cols; i++){
-//     const block = document.createElement('div');
-//     block.classList.add('block');
-//     board.appendChild(block);
-// }
-
-for (let row = 0; row<rows; row++){
-    for (let col = 0; col<cols; col++){
-            const block = document.createElement('div');
-            block.classList.add('block');
-            board.appendChild(block);
-            // block.innerHTML = `${row}-${col}`
-            blocks[`${row}-${col}`] = block;
-    };
-};
+let highscore = localStorage.getItem("highscore") || 0;
+highscoreElement.textContent = highscore
+let score = 0;
+let time = `00-00`;
 
 
-function render() { //snake jaha bhi rahega render hojaega
+// loop
+for(let row = 0; row<rows; row++){
+    for(let col = 0; col<cols; col++){
+        let block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block)
+
+        // block.innerText = `${row}-${col}`
+
+        blocks[`${row}-${col}`] = block;
+    }
+}
+
+function timerFunction() {
+        timerintervalId = setInterval(() => {
+            let [min, sec] = time.split("-").map(Number);
+            if (sec === 59) {
+                sec = 0;
+                min+= 1
+            }else{
+                sec+= 1;
+            }
+
+            time = `${min}-${sec}`
+            timeElement.textContent = time;
+}, 1000);
+}
+
+
+function render() {
 
     let head = null;
 
     blocks[`${food.x}-${food.y}`].classList.add('food')
 
-
-    if (direction === 'left') {
-        head = {x: snake[0].x, y: snake[0].y - 1}
-    }else if(direction === 'right'){
-        head = {x: snake[0].x, y: snake[0].y + 1}
-    }else if(direction === 'down'){
-        head = {x: snake[0].x + 1, y: snake[0].y}
-    }else if(direction === 'up'){
-        head = {x: snake[0].x  - 1, y: snake[0].y}
+    if(direction === 'up'){
+        head = {x: snake[0].x - 1, y: snake[0].y}
     }
 
-    // wall collison logic
-    if (head.x <0 || head.x >= rows || head.y <0 || head.y >=cols) {
-        clearInterval(intervalId)
+    else if(direction === 'down'){
+        head = {x: snake[0].x + 1, y: snake[0].y}
+    }
 
-        modal.style.display = "flex"
+    else if(direction === 'left'){
+        head = {x: snake[0].x, y: snake[0].y - 1}
+    }
+
+    else if(direction === 'right'){
+        head = {x: snake[0].x, y: snake[0].y + 1}
+    }
+    
+
+    //game over logic
+    if (head.x >= rows || head.x < 0 || head.y >= cols || head.y < 0 ) {
+        clearInterval(intervalId)
+        clearInterval(timerintervalId)
+
+        modal.style.display = "flex";
         startGameModal.style.display = "none"
-        gameOverModal.style.display = "flex"
+        endGameModal.style.display = "flex"
 
         return;
     }
 
+    // if(snake.some(segment=>head.x === segment.x && head.y === segment.y)){
+    //     clearInterval(intervalId)
+    //     clearInterval(timerintervalId)
+    //     modal.style.display = "flex";
+    //     startGameModal.style.display = "none"
+    //     endGameModal.style.display = "flex"
+    //     return;
+    // }
+
+
     // food consume logic
-    if (head.x == food.x && head.y == food.y) {
+    if (head.x === food.x && head.y === food.y) {
         blocks[`${food.x}-${food.y}`].classList.remove('food')
-        food = {
-             x: Math.floor(Math.random()*rows),y: Math.floor(Math.random()*cols)
-        }
-        blocks[`${food.x}-${food.y}`].classList.add('food')
+        food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)}
 
-        snake.unshift(head);
+        snake.unshift(head)
 
-        score += 10;
+        score +=1;
         scoreElement.textContent = score;
 
-        if (score > highScore) {
-            highScore = score
-            localStorage.setItem("highScore", highScore.toString())
-            highscoreElement.textContent = highScore;
+        if (score>highscore) {
+            highscore = score
+            localStorage.setItem("highscore",highscore.toString())
+            highscoreElement.textContent = highscore
         }
     }
 
     snake.forEach((segment)=>{
-        blocks[`${segment.x}-${segment.y}`].classList.remove('fill') 
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill')
+        blocks[`${segment.x}-${segment.y}`].classList.remove('head')
     })
 
     snake.unshift(head)
     snake.pop()
 
-
-
-
-    snake.forEach((segment)=>{
-        blocks[`${segment.x}-${segment.y}`].classList.add('fill')
-        
+    snake.forEach((segment, idx)=>{
+        if (idx === 0) {
+            blocks[`${segment.x}-${segment.y}`].classList.add('head')
+        }else{
+            blocks[`${segment.x}-${segment.y}`].classList.add('fill')
+        }       
     })
 }
 
 
-startButton.addEventListener("click", () => {
+let rendertime = null;
+document.querySelector(".start-btn").addEventListener("click",()=>{
+
     modal.style.display = 'none';
-    intervalId = setInterval(() => { render() }, 300);
+    gameDifficulty.style.display = 'flex';
 
-    timerintervalId = setInterval(()=>{
-        let [min, sec] = time.split("-").map(Number)
+    easybtn.addEventListener("click", ()=>{ gameDifficulty.style.display = 'none'; intervalId = setInterval(() => { render()}, 300); rendertime=300})
 
-        if (sec=== 59) {
-            min += 1;
-            sec = 0;
-        }else{
-            sec+= 1;
-        }
-        time = `${min}-${sec}`
-        timeElement.innerHTML = time
-    },1000)
+    mediumbtn.addEventListener("click", ()=>{ gameDifficulty.style.display = 'none'; intervalId = setInterval(() => { render()}, 200); rendertime=200})
+
+    hardbtn.addEventListener("click", ()=>{ gameDifficulty.style.display = 'none'; intervalId = setInterval(() => { render()}, 100); rendertime=100})
 
 })
 
-restartButton.addEventListener("click", restartGame)
 
-function restartGame() {
-    blocks[`${food.x}-${food.y}`].classList.remove('food')
+document.querySelector(".restart-btn").addEventListener("click",()=>{
+    modal.style.display = 'none';
+
+    blocks[`${food.x}-${food.y}`].classList.remove('food');
     snake.forEach((segment)=>{
-        blocks[`${segment.x}-${segment.y}`].classList.remove('fill') 
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill'); 
+        blocks[`${segment.x}-${segment.y}`].classList.remove('head'); 
     })
+
     score = 0;
-    time = `00:00`;
+    time = `00-00`;
 
     scoreElement.textContent = score;
-    timeElement.textContent = time;
-    highscoreElement.textContent = highScore;
+    timeElement.textContent = parseInt(time, 10)
 
-
-    modal.style.display = 'none';
-    direction = 'down'
-    snake = [ { x: 1, y: 3} ];
+    direction = 'down';
+    snake = [{x: 1,y: 3}]
     food = {x: Math.floor(Math.random()*rows),y: Math.floor(Math.random()*cols)};
-    intervalId = setInterval(() => { render() }, 300);
-
-    
-    
-}
 
 
-addEventListener("keydown", (event)=>{
-    
-    if (event.key == 'w') {
-        direction = "up"
-    }else if (event.key == 's') {
-        direction = "down"
-    }else if (event.key == 'a') {
-        direction = "left"
-    }else if (event.key == 'd') {
-        direction = "right"
-    }
+    timerFunction()
+
+    intervalId = setInterval(() => { render()}, rendertime);
 })
 
-//phone
-let touchStartX = 0;
-let touchStartY = 0;
 
-addEventListener("touchstart", (event) => {
-    touchStartX = event.touches[0].clientX;
-    touchStartY = event.touches[0].clientY;
-}, { passive: true });
+// direction change logic based on user input
+addEventListener("keydown",(evt)=>{
 
-addEventListener("touchend", (event) => {
-    const touchEndX = event.changedTouches[0].clientX;
-    const touchEndY = event.changedTouches[0].clientY;
-
-    const diffX = touchEndX - touchStartX;
-    const diffY = touchEndY - touchStartY;
-
-    const minSwipeDistance = 20; // ignore tiny accidental touches
-
-    if (Math.abs(diffX) < minSwipeDistance && Math.abs(diffY) < minSwipeDistance) {
-        return; // too small to count as a swipe
+    if (evt.key === 'ArrowUp' || evt.key === 'w') {
+        direction = 'up'
     }
-
-    if (Math.abs(diffX) > Math.abs(diffY)) {
-        // horizontal swipe
-        direction = diffX > 0 ? "right" : "left";
-    } else {
-        // vertical swipe
-        direction = diffY > 0 ? "down" : "up";
+    else if (evt.key === 'ArrowDown' || evt.key === 's') {
+        direction = 'down'
     }
-}, { passive: true });
+    else if (evt.key === 'ArrowLeft' || evt.key === 'a') {
+        direction = 'left'
+    }
+    else if (evt.key === 'ArrowRight' || evt.key === 'd') {
+        direction = 'right'
+    }
+    
+})
